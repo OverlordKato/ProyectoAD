218dc0acc0b55839c7e612097cd250429d099c15
Ellie Colorado <123810583+Elrecoal@users.noreply.github.com>
2024-02-14 17:22:16 +0100

    Añadida estructura de roles (hay que cambiar el campo en el signup por un dropdown)

--- NodeJsMongoDBCrudLogin/models/user.js
+++ NodeJsMongoDBCrudLogin/models/user.js
@@ -4,33 +4,34 @@ const bcrypt = require('bcrypt-nodejs');
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-  email:  { type: String, required: true },
-  password:  { type: String, required: true }
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  rol: { type: String, required: true }
 });
 
 userSchema.methods.encryptPassword = (password) => {
   return bcrypt.hashSync(password, bcrypt.genSaltSync(10));
 };
 
-userSchema.methods.comparePassword= function (password) {
+userSchema.methods.comparePassword = function (password) {
   return bcrypt.compareSync(password, this.password);
 };
 
 userSchema.methods.encryptPassword = (password) => {
   return bcrypt.hashSync(password, bcrypt.genSaltSync(10));
 };
-userSchema.methods.comparePassword= function (password) {
+userSchema.methods.comparePassword = function (password) {
   return bcrypt.compareSync(password, this.password);
 };
 
-userSchema.methods.findEmail= async (email) => {
+userSchema.methods.findEmail = async (email) => {
   const User = mongoose.model("user", userSchema);
-  return  await User.findOne({'email': email})
+  return await User.findOne({ 'email': email })
 
 };
 
 
-userSchema.methods.insert= async function () {
+userSchema.methods.insert = async function () {
   //await this.save();
   await this.save((err, res) => {
     err ? console.log(err) : "";
